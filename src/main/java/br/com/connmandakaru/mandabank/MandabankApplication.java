ab17ea30e5b617e14aa0f499e71db727c02368fd
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2025-12-23 10:21:53 -0300

    feat: add initial configuration

---
+++ src/main/java/br/com/connmandakaru/mandabank/MandabankApplication.java
@@ -0,0 +1,13 @@
+package br.com.connmandakaru.mandabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandabankApplication.class, args);
+	}
+
+}
