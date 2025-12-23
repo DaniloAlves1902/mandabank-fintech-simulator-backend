014e397398b4356aab7acd25ae308fd2121c2b13
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2025-12-23 15:29:07 -0300

    fix: correct return type of findByUsername to UserDetails in UserRepository

--- src/main/java/br/com/connmandakaru/mandabank/repositories/UserRepository.java
+++ src/main/java/br/com/connmandakaru/mandabank/repositories/UserRepository.java
@@ -2,13 +2,19 @@ package br.com.connmandakaru.mandabank.repositories;
 
 import br.com.connmandakaru.mandabank.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<String> findByEmail (String email);
-    boolean existsByEmail (String email);
+
+    UserDetails findByEmail(String email);
+
+    User findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String email);
 }
