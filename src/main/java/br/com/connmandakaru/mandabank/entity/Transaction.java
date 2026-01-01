9b83c6870b83deac84debe87b46a912dc8ff29ff
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2026-01-01 15:59:41 -0300

    refactor: change the attribute type to String.

--- src/main/java/br/com/connmandakaru/mandabank/entity/Transaction.java
+++ src/main/java/br/com/connmandakaru/mandabank/entity/Transaction.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private String id;
 
     @NotNull
     @Version
